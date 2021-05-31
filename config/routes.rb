5e04ab085442e52077a2ebd2b4387466bb4bb2a8
joao sampaio <joaomarcosemgp@gmail.com>
2021-05-31 18:16:35 -0300

    primeiro pull merge

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'cars#home'
+  resources :cars, only: [:show, :new, :create] do
+    resources :rents, only: [:create]
+  end
+
 end
