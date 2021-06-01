0caa42a21f8fd8c81e7fdfa1951cd4da343477de
dudu carneiro <dumaniaoctavio@hotmail.com>
2021-06-01 16:23:32 -0300

    faker gem and seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+Car.destroy_all
+puts 'Creating 20 fake cars...'
+
+
+20.times do
+  car = Car.new(
+    model:    Faker::Vehicle.make_and_model,
+    year:    rand(1950..2021),
+    km:    rand(12_000..180_000),
+    location:    "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    user_id: 5
+  )
+  car.save!
+  puts "#{car.id} #{car.model}"
+end
+puts 'Finished!'
