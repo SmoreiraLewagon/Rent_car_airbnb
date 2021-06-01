df8e8c21b29e3f26624d773f107850a2fdf50acd
Benjamin Coccoli <benji.coccoli@gmail.com>
2021-06-01 14:43:14 -0300

    added the basics of the html new + home

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,3 +1,7 @@
 class Car < ApplicationRecord
   belongs_to :user
+  validates :model, presence: true
+  validates :year, presence: true
+  validates :location, presence: true
+  validates :km, presence: true
 end
