7ca142812f812b6fe4fdf8f8a98dc4f940cb6ebb
dudu carneiro <dumaniaoctavio@hotmail.com>
2021-06-01 13:04:47 -0300

    column admin users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,8 +12,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:user_name])
   end
 
-  after_action :verify_authorized, except: :index, unless: :skip_pundit?
-  after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
+  after_action :verify_authorized, except: :home, unless: :skip_pundit?
+  after_action :verify_policy_scoped, only: :home, unless: :skip_pundit?
   
   private
 
