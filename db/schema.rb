4ce98066c1a6bda45aa9396c911794b1940f8f1b
Benjamin Coccoli <benji.coccoli@gmail.com>
2021-06-02 14:54:58 -0300

    css on the home page + banner

--- db/schema.rb
+++ db/schema.rb
@@ -73,7 +73,7 @@ ActiveRecord::Schema.define(version: 2021_06_01_225344) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.boolean "admin"
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
