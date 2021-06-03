f1bd65f38991cb3595fc313feef83871a5a86520
dudu carneiro <dumaniaoctavio@hotmail.com>
2021-06-02 21:25:44 -0300

    custom icon & marker

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -8,13 +8,23 @@ const fitMapToMarkers = (map, markers) => {
     const bounds = new mapboxgl.LngLatBounds();
     markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
     map.fitBounds(bounds, { padding: 50, maxZoom: 20, duration: 3000 });
+
   };
 
 const addMarkersToMap = (map, markers) => {
     markers.forEach((marker) => {
       const popup = new mapboxgl.Popup().setHTML(marker.info_window); // add this
   
-      new mapboxgl.Marker()
+      const element = document.createElement('div');
+      element.className = 'marker';
+      element.style.backgroundImage = `url('${marker.image_url}')`;
+      element.style.backgroundSize = 'contain';
+      element.style.width = '25px';
+      element.style.height = '25px';
+
+
+
+      new mapboxgl.Marker(element)
         .setLngLat([ marker.lng, marker.lat ])
         .setPopup(popup) // add this
         .addTo(map);
@@ -31,18 +41,19 @@ const initMapbox = () => {
       style: 'mapbox://styles/dumania/ckpfriohw0lcy18k1us8tt3tk'
     });
     
-    const markers = JSON.parse(mapElement.dataset.markers);
-    markers.forEach((marker) => {
-      new mapboxgl.Marker()
-        .setLngLat([ marker.lng, marker.lat ])
-        .addTo(map);
-    });
+    map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
+      mapboxgl: mapboxgl }));
 
+    const markers = JSON.parse(mapElement.dataset.markers);
+    
+
+
+
+      
     addMarkersToMap(map, markers);
     fitMapToMarkers(map, markers);
 
-    map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
-        mapboxgl: mapboxgl }));
+
 
   }
 };
